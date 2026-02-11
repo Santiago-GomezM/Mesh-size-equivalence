19419ca2f4eae2460f487364af347499b6bee4bb
Santiago-GomezM <162300429+Santiago-GomezM@users.noreply.github.com>
2026-02-11 17:37:00 +0100

    Delete src/prob_ellipse_hex.m

--- src/prob_ellipse_hex.m
+++
@@ -1,8 +0,0 @@
-function P = prob_ellipse_hex(p, a, b)
-
-A_H = 2*sqrt(3)*p^2;   % área del hexágono regular de apotema p
-g = @(theta) 1/pi;     % orientación uniforme
-integrand = @(theta) g(theta) .* arrayfun(@(t) area_Dtheta_hex(p,a,b,t), theta);
-P = (1/A_H) * integral(integrand, 0, pi, 'AbsTol',1e-8,'RelTol',1e-6);
-
-end
